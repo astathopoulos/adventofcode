19a7178383e48462929606db25c03b802a799b0c
Tasos Stathopoulos <stathopa@skroutz.gr>
2015-12-22 01:14:00 +0200

    Add day 13 solutions

---
+++ day_13/quiz_1.rb
@@ -0,0 +1,16 @@
+$points = {}
+File.open('input.txt', 'r').each_line do |line|
+  name_1, mood, points, name_2 =
+    line.strip.scan(/(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+)\./).flatten
+  $points[[name_1, name_2]] = mood == 'gain' ? points.to_i : -points.to_i
+end
+
+$people = $points.keys.flatten.uniq
+max_happiness = $points.keys.flatten.uniq.permutation($people.size).map { |people|
+  people.unshift(people[-1])
+  people.each_cons(2).map { |pair|
+    $points[[*pair]] + $points[[*pair.reverse]]
+  }.inject(:+)
+}.max
+
+p max_happiness
