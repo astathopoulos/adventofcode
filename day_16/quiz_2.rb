70d070f7eb1e6f2d96682a28f60fce0b033bd599
Tasos Stathopoulos <stathopa@skroutz.gr>
2015-12-22 02:00:47 +0200

    Add day 16 solutions

---
+++ day_16/quiz_2.rb
@@ -0,0 +1,27 @@
+$aunts = {}
+$findings = {
+  children: [:==, 3],
+  cats: [:>, 7],
+  samoyeds: [:==, 2],
+  pomeranians: [:<, 3],
+  akitas: [:==, 0],
+  vizslas: [:==, 0],
+  goldfish: [:<, 5],
+  trees: [:>, 3],
+  cars: [:==, 2],
+  perfumes: [:==, 1]
+}
+
+File.open('input.txt', 'r').each_line do |line|
+  aunt = line.strip.scan(/Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)$/).flatten
+
+  $aunts[aunt[0]] = {
+    aunt[1].to_sym => aunt[2].to_i,
+    aunt[3].to_sym => aunt[4].to_i,
+    aunt[5].to_sym => aunt[6].to_i
+  }
+end
+
+p $aunts.map { |name, attrs|
+  attrs.keys.map { |key| attrs[key].send(*$findings[key]) }.all? ? name : nil
+}.compact.first.to_i
